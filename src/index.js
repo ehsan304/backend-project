407cb7f82d8915168905e4b56876763a400bf108
ehsan304 <muhammadehsan304@gmail.com>
2024-04-27 04:25:29 +0500

    correct some mistakes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import connectDB from "./db/index.js";
 import {app} from './app.js'
 
 dotenv.config({
-    path: "./env"
+    path: "./.env"
 })
 
 
@@ -14,8 +14,8 @@ dotenv.config({
 
 connectDB()
     .then(() => {
-        app.listen(process.env.PORT || 8000, () => {
-            console.log(`Serveris running at port : ${process.env.PORT}`)
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is running at port : ${process.env.PORT}`)
         })
     })
     .catch((err) => {
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-const router = Router()
+const router = Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(asyncHandler(registerUser));
 
-
-
-export default router
+export default router;
